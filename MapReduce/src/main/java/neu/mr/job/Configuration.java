60001e22570db5249179a9d06fe9b376910cd89b
chintanpathak <pathak.c@husky.neu.edu>
2016-04-17 16:19:00 -0400

    Changing config

--- MapReduce/src/main/java/neu/mr/job/Configuration.java
+++ MapReduce/src/main/java/neu/mr/job/Configuration.java
@@ -9,23 +9,18 @@ import java.util.Map;
  * @author Mania Abdi
  *
  */
-public class Configuration implements Serializable{
+public class Configuration implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	Map <String, Object> map = new HashMap<String, Object>();	
-	
-	public void set (String name, Object value)
-	{
+	Map<String, Object> map = new HashMap<String, Object>();
+
+	public void set(String name, Object value) {
 		map.put(name, value);
 	}
-	
-	public Object getValue (String key)
-	{
-		if (map.containsKey(key))
-			return null;
-		
-		return map.get(key);
+
+	public Object getValue(String key) {
+		return map.containsKey(key) ? map.get(key) : null;
 	}
-	
+
 }
